aa2d0d95fe5095ccc13d22cce0361a0b4b11f9ba
Siddhesh Kanse <42892993+17101A0031@users.noreply.github.com>
2026-03-03 23:51:08 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are learning Branching"
 echo "I am learning Git"
 echo "I am learning Devops"
-a=5
-echo "I learn it"
-echo "My name is Soham"
+
