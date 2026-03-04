30bda41c12ff20b4b1f1604ff9582585e137c8ca
ksiddhesh99@gmail.com <ksiddhesh99@gmail.com>
2026-03-04 11:30:39 +0530

    S1

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,8 @@
 echo "We are trying this practical from linux EC2"
 echo "We are learning Branching"
 echo "I am learning Git and Githubggggggggggggggtyy"
-echo "Modified Line added"
+echo "dfgdgdgdgdgd"
 echo "This is my First Prac"
 echo "This is my SecondPrac"
+echo "This is my Third Prac"
 
